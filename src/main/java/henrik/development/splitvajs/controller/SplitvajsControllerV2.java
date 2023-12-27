2b0f9ae3356c5834bc0689f93fa5ef92d1bd8c14
henriksimons <henriksimons2@gmail.com>
2023-12-27 10:23:12 +0100

    Add CrossOrigin annotation

--- src/main/java/henrik/development/splitvajs/controller/SplitvajsControllerV2.java
+++ src/main/java/henrik/development/splitvajs/controller/SplitvajsControllerV2.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Objects;
 
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RestController
 @RequestMapping("/v2")
 public class SplitvajsControllerV2 {
