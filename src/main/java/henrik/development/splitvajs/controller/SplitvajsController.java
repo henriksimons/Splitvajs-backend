4e14ca37b801cc40f0006b54b86eaa85f96b57ac
henriksimons <henriksimons2@gmail.com>
2023-12-28 18:47:16 +0100

    Add cors url

--- src/main/java/henrik/development/splitvajs/controller/SplitvajsController.java
+++ src/main/java/henrik/development/splitvajs/controller/SplitvajsController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Objects;
 
-@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
+@CrossOrigin(origins = {"http://localhost:3000", "https://henriksimons.github.io/Splitvajs"}, maxAge = 3600)
 @RestController
 public class SplitvajsController {
     private final SplitvajsServiceImpl service;
